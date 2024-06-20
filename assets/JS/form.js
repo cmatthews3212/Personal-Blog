d3d475269262b2475fca18aa4736fd085c7656fc
Christine Matthews <candreola@gmail.com>
2024-06-20 11:22:48 -0500

    Store form values to local storage

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -2,10 +2,25 @@ const userInput = document.getElementById("username");
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
 
-console.log(userInput, titleInput, contentInput)
+
+
+const values = localStorage.getItem('Form Values');
+
+
+
+// console.log(userInput, titleInput, contentInput)
 
 const redirect = function () {
     event.preventDefault();
+   
+    const values = {
+        username: userInput.value,
+        title: titleInput.value,
+        content: contentInput.value,
+    };
+
+    localStorage.setItem('Form Values', JSON.stringify(values));
+
     window.location.href = "blog.html"
-}
+};
 
