1fbe4675e11cac9edf65b5764d4bc9cbca7c4c6e
Christine Matthews <candreola@gmail.com>
2024-06-21 14:25:11 -0500

    New attempt to save data to page

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -8,29 +8,40 @@ const submitButton = document.getElementById("submit");
 
 
 // const values = 
-
-
+// let valuesArray = [];
 
 
 function redirect (event) {
     event.preventDefault();
-    let valuesArray = JSON.parse(localStorage.getItem('Form Values'));
-
+   let  valuesArray = [JSON.parse(localStorage.getItem('Form Values'))];
     // Virtual assistent helped with this formadata function
-    const formData = new FormData(form);
     let values = {};
+    const formData = new FormData(form);
+    
 
     
         formData.forEach(function (value, key) {
             values[key] = value;
         })
 
-    valuesArray.push(values)
-    console.log(valuesArray);
+
+        
+      
+    
+        valuesArray.push(values)
+        
+        if (valuesArray[0] == null) {
+            valuesArray.shift()
+        }
+   
+   
     localStorage.setItem('Form Values', JSON.stringify(valuesArray));
-
-
+    
     location.href = "./blog.html"
+    
+    
+    
 };
+
 
 submitButton.addEventListener('click', redirect);
